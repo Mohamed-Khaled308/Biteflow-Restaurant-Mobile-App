d657373e4b6136cbb2159cfd3f05d5a7c1be3437
AhmadHoseiny <ahmadshahhat9898@gmail.com>
2024-11-20 01:42:39 +0200

    fixed deprecation issue

--- lib/views/theme/theme_data.dart
+++ lib/views/theme/theme_data.dart
@@ -24,12 +24,12 @@ const AppBarTheme appBarDarkTheme = AppBarTheme(
 );
 
 ScrollbarThemeData scrollbarThemeData = ScrollbarThemeData(
-  trackColor: MaterialStateProperty.all(ThemeConstants.primaryColor),
+  trackColor: WidgetStateProperty .all(ThemeConstants.primaryColor),
 );
 
 DataTableThemeData dataTableLightThemeData = DataTableThemeData(
   columnSpacing: 24,
-  headingRowColor: MaterialStateProperty.all(Colors.black12),
+  headingRowColor: WidgetStateProperty .all(Colors.black12),
   decoration: BoxDecoration(
     borderRadius: const BorderRadius.all(Radius.circular(ThemeConstants.defaultBorderRadious)),
     border: Border.all(color: Colors.black12),
@@ -43,7 +43,7 @@ DataTableThemeData dataTableLightThemeData = DataTableThemeData(
 
 DataTableThemeData dataTableDarkThemeData = DataTableThemeData(
   columnSpacing: 24,
-  headingRowColor: MaterialStateProperty.all(Colors.white10),
+  headingRowColor: WidgetStateProperty .all(Colors.white10),
   decoration: BoxDecoration(
     borderRadius: const BorderRadius.all(Radius.circular(ThemeConstants.defaultBorderRadious)),
     border: Border.all(color: Colors.white10),
