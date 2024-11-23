1e29d3c2dc95afe2e33c826546c764d74b8f54d4
Mohamed Hassan <m.hassan@advanced-quantum.de>
2024-11-23 15:42:33 +0200

    Add dummy restaurant data and HomeViewModel

---
+++ lib/view-model/home_view_model.dart
@@ -0,0 +1,26 @@
+
+import 'package:biteflow/dummy_data/dummy_restaurants.dart';
+import 'package:flutter/foundation.dart';
+import '../models/restaurant.dart';
+// import '../data/dummy_restaurants.dart';
+
+
+class HomeViewModel extends ChangeNotifier {
+  List<Restaurant> bestPickRestaurants = [];
+  List<Restaurant> allRestaurants = [];
+
+  HomeViewModel() {
+    loadRestaurants();
+  }
+
+  void loadRestaurants() {
+    bestPickRestaurants = bestPickRestaurantsDate;
+    allRestaurants = allRestaurantsData;
+    notifyListeners();
+  }
+
+  
+  Future<void> fetchRestaurantsFromDatabase() async {
+    // TODO: fectch the data from the database
+  }
+}
