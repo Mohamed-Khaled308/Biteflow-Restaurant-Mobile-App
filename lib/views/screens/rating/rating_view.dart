ea6bddadf40012e8499f37863b6519f479bf3ebb
Mohamed Hassan <mohamedhassan2082002@hotmail.com>
2024-11-24 21:01:08 +0200

    feat: add navigateAndRemove method and update rating widget

--- lib/views/screens/rating/rating_view.dart
+++ lib/views/screens/rating/rating_view.dart
@@ -1,14 +1,17 @@
 import 'package:biteflow/constants/theme_constants.dart';
+import 'package:biteflow/locator.dart';
 import 'package:biteflow/models/order_item.dart';
+import 'package:biteflow/services/navigation_service.dart';
 import 'package:biteflow/view-model/rating_view_model.dart';
 import 'package:biteflow/views/screens/home/home_view.dart';
 import 'package:biteflow/views/widgets/rating/rating_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
- 
 
 class RatingView extends StatelessWidget {
-  const RatingView({super.key});
+  RatingView({super.key});
+
+  final TextEditingController _feedbackController = TextEditingController();
 
   void _showRatingBottomSheet(
       BuildContext context, OrderItem item, RatingViewModel viewModel) {
@@ -17,7 +20,8 @@ class RatingView extends StatelessWidget {
       isScrollControlled: true, // To make the bottom sheet full height
       builder: (context) {
         return FractionallySizedBox(
-          heightFactor: 0.8,
+          heightFactor: 0.3,
+          widthFactor: 1.0,
           child: RatingBottomSheet(
             item: item,
             onRatingSubmitted: (rating) {
@@ -27,14 +31,6 @@ class RatingView extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-
-  void _navigateToHome(BuildContext context) {
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (context) => const HomeView()),
-      (Route<dynamic> route) => false,
     );
   }
 
@@ -55,66 +51,99 @@ class RatingView extends StatelessWidget {
                   child: ListView.builder(
                     padding:
                         const EdgeInsets.all(ThemeConstants.defaultPadding),
-                    itemCount: viewModel.orderedItems.length,
+                    itemCount: viewModel.orderedItems.length +
+                        1, // Add 1 for the footer
                     itemBuilder: (context, index) {
-                      final item = viewModel.orderedItems[index];
-                      return Card(
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 8.0,
-                          horizontal: 4.0,
-                        ),
-                        child: ListTile(
-                          leading: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.network(
-                              item.imageUrl,
-                              width: 60,
-                              height: 60,
-                              fit: BoxFit.cover,
+                      if (index == viewModel.orderedItems.length) {
+                        // Footer: Feedback and Submit Button
+                        return Padding(
+                          padding: const EdgeInsets.all(
+                              ThemeConstants.defaultPadding),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              const SizedBox(height: 16),
+                              TextField(
+                                controller: _feedbackController,
+                                decoration: InputDecoration(
+                                  labelText: 'Leave a comment (optional)',
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        ThemeConstants.defaultBorderRadious),
+                                  ),
+                                ),
+                                maxLines: 3,
+                              ),
+                              const SizedBox(height: 16),
+                              ElevatedButton(
+                                onPressed: () {
+                                  // TODO: Handle feedback submission
+                                  // For example: saveFeedback(_feedbackController.text);
+                                  print(
+                                      'Feedback: ${_feedbackController.text}');
+                                  getIt<NavigationService>().navigateAndRemove(
+                                    const HomeView(),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: ThemeConstants.primaryColor,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        ThemeConstants.defaultBorderRadious),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 12.0,
+                                    horizontal: 24.0,
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Submit',
+                                  style: TextStyle(
+                                    color: ThemeConstants.whiteColor,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        // Regular list item
+                        final item = viewModel.orderedItems[index];
+                        return Card(
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 8.0,
+                            horizontal: 4.0,
+                          ),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.network(
+                                item.imageUrl,
+                                width: 60,
+                                height: 60,
+                                fit: BoxFit.cover,
+                              ),
                             ),
+                            title: Text(item.title),
+                            subtitle: Text(
+                              'Quantity: ${item.quantity}\n${item.description}',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            trailing: Icon(
+                              Icons.star,
+                              color: item.rating > 0
+                                  ? ThemeConstants.warningColor
+                                  : ThemeConstants.greyColor,
+                            ),
+                            onTap: () {
+                              _showRatingBottomSheet(context, item, viewModel);
+                            },
                           ),
-                          title: Text(item.title),
-                          subtitle: Text(
-                            'Quantity: ${item.quantity}\n${item.description}',
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          trailing: Icon(
-                            Icons.star,
-                            color: item.rating > 0
-                                ? ThemeConstants.warningColor
-                                : ThemeConstants.greyColor,
-                          ),
-                          onTap: () {
-                            _showRatingBottomSheet(context, item, viewModel);
-                          },
-                        ),
-                      );
+                        );
+                      }
                     },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(ThemeConstants.defaultPadding),
-                  child: ElevatedButton(
-                    onPressed: () => _navigateToHome(context),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: ThemeConstants.primaryColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            ThemeConstants.defaultBorderRadious),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 12.0,
-                        horizontal: 24.0,
-                      ),
-                    ),
-                    child: const Text(
-                      'Return to Home',
-                      style: TextStyle(
-                        color: ThemeConstants.whiteColor,
-                        fontSize: 16,
-                      ),
-                    ),
                   ),
                 ),
               ],
--- lib/views/widgets/rating/rating_bottom_sheet.dart
+++ lib/views/widgets/rating/rating_bottom_sheet.dart
@@ -22,7 +22,7 @@ class RatingBottomSheet extends StatefulWidget {
 
 class _RatingBottomSheetState extends State<RatingBottomSheet> {
   double _currentRating = 0.0;
-  final TextEditingController _feedbackController = TextEditingController();
+
 
   @override
   void initState() {
@@ -32,85 +32,51 @@ class _RatingBottomSheetState extends State<RatingBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        top: ThemeConstants.defaultPadding,
-        left: ThemeConstants.defaultPadding,
-        right: ThemeConstants.defaultPadding,
-        bottom: MediaQuery.of(context).viewInsets.bottom +
-            ThemeConstants.defaultPadding,
-      ),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Rate ${widget.item.title}',
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.network(
-                widget.item.imageUrl,
-                width: 120,
-                height: 120,
-                fit: BoxFit.cover,
-              ),
-            ),
-            const SizedBox(height: 16),
-            StarRatingBar(
-              initialRating: _currentRating,
-              onRatingUpdate: (rating) {
-                setState(() {
-                  _currentRating = rating;
-                });
-              },
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _feedbackController,
-              decoration: InputDecoration(
-                labelText: 'Leave a comment (optional)',
-                border: OutlineInputBorder(
-                  borderRadius:
-                      BorderRadius.circular(ThemeConstants.defaultBorderRadious),
-                ),
-              ),
-              maxLines: 3,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                widget.onRatingSubmitted(_currentRating);
-                // TODO: Handle feedback 
-                // For example: saveFeedback(_feedbackController.text);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: ThemeConstants.primaryColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                      ThemeConstants.defaultBorderRadious),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  vertical: 12.0,
-                  horizontal: 24.0,
-                ),
-              ),
-              child: const Text(
-                'Submit',
-                style: TextStyle(
-                  color: ThemeConstants.whiteColor,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
+    return  Padding(
+        padding: EdgeInsets.only(
+          top: ThemeConstants.defaultPadding,
+          left: ThemeConstants.defaultPadding,
+          right: ThemeConstants.defaultPadding,
+          bottom: MediaQuery.of(context).viewInsets.bottom +
+              ThemeConstants.defaultPadding,
         ),
-      ),
-    );
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Rate ${widget.item.title}',
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  widget.item.imageUrl,
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 16),
+              StarRatingBar(
+                initialRating: _currentRating,
+                onRatingUpdate: (rating) {
+                  setState(() {
+                    _currentRating = rating;
+                    widget.onRatingSubmitted(_currentRating);
+                  });
+                },
+              ),
+              
+             
+            ],
+          ),
+        ),
+      );
+    
   }
 }
