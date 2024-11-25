0c92b39b1a12f8a7fd3d64dbb8260036a7fdf8d4
youssef1shawky <youssefshawky2002@gmail.com>
2024-11-26 00:45:37 +0200

    Splash Screen

---
+++ lib/animated_splash_screen.dart
@@ -0,0 +1,89 @@
+import 'package:biteflow/locator.dart';
+import 'package:biteflow/services/navigation_service.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AnimatedSplashScreen extends StatefulWidget {
+  final Widget nextScreen;
+  
+  const AnimatedSplashScreen({
+    super.key,
+    required this.nextScreen,
+  });
+
+  @override
+  State<AnimatedSplashScreen> createState() => _AnimatedSplashScreenState();
+}
+
+class _AnimatedSplashScreenState extends State<AnimatedSplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2500),
+      vsync: this,
+    );
+
+    _rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 2 * math.pi,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.7, curve: Curves.easeInOut),
+      ),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 2,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.7, 1.0, curve: Curves.easeInOut),
+      ),
+    );
+
+    _controller.forward().then((_) {
+      getIt<NavigationService>().navigateAndRemove(widget.nextScreen);
+      
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _scaleAnimation.value,
+              child: Transform.rotate(
+                angle: _rotationAnimation.value,
+                child: Image.asset(
+                  'assets/images/splash/logo.jpeg',
+                  width: 300,
+                  height: 300,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
