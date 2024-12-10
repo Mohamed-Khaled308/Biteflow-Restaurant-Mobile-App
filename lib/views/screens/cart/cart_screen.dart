0cdd3e92c0424c6fe9ca138612434b14d48fb41d
youssef1shawky <youssefshawky2002@gmail.com>
2024-12-10 20:01:38 +0200

    scanning the QR code

--- lib/views/screens/cart/cart_screen.dart
+++ lib/views/screens/cart/cart_screen.dart
@@ -1,7 +1,10 @@
+import 'package:biteflow/locator.dart';
+import 'package:biteflow/services/navigation_service.dart';
 import 'package:biteflow/viewmodels/cart_view_model.dart';
 import 'package:biteflow/views/widgets/cart/card.dart';
 import 'package:biteflow/views/widgets/cart/payment_summary.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -22,12 +25,47 @@ class _CartScreenState extends State<CartScreen> {
         title: const Text('Cart'),
         actions: [
           IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text(
+                      'Scan QR Code to be added to cart',
+                      textAlign: TextAlign.center,
+                    ),
+                    content: Container(
+                      height: 300,
+                      width: 300,
+                      alignment: Alignment.center,
+                      child: MobileScanner(
+                        controller: MobileScannerController(
+                          detectionSpeed: DetectionSpeed.normal,
+                        ),
+                        onDetect: (capture) {
+                          final List<Barcode> barcodes = capture.barcodes;
+                          for (Barcode barcode in barcodes) {
+                            if (barcode.rawValue != null) {
+                              print("barcode value is ${barcode.rawValue}");
+                              getIt<NavigationService>().pop();// todo redirect to the new cart
+                            }
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.camera_alt_rounded)),
+          IconButton(
               icon: const Icon(Icons.qr_code_2_rounded),
               onPressed: () {
                 showDialog(
                   context: context,
                   builder: (ctx) => AlertDialog(
-                    title: const Text('Scan QR Code to be added to cart' , textAlign: TextAlign.center,),
+                    title: const Text(
+                      'Scan this QR Code to be added to the order',
+                      textAlign: TextAlign.center,
+                    ),
                     content: Container(
                       height: 200,
                       width: 200,
@@ -40,6 +78,7 @@ class _CartScreenState extends State<CartScreen> {
                     ),
                   ),
                 );
+             
               })
         ],
       ),
