e99eabca3a502d6afec2eeb852ae6784172562eb
youssef1shawky <youssefshawky2002@gmail.com>
2024-12-10 16:49:57 +0200

    generating QR code

--- lib/views/screens/cart/cart_screen.dart
+++ lib/views/screens/cart/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:biteflow/views/widgets/cart/card.dart';
 import 'package:biteflow/views/widgets/cart/payment_summary.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -19,6 +20,28 @@ class _CartScreenState extends State<CartScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Cart'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.qr_code_2_rounded),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Scan QR Code to be added to cart' , textAlign: TextAlign.center,),
+                    content: Container(
+                      height: 200,
+                      width: 200,
+                      alignment: Alignment.center,
+                      child: QrImageView(
+                        data: '1234567890',
+                        version: QrVersions.auto,
+                        size: 200.0,
+                      ),
+                    ),
+                  ),
+                );
+              })
+        ],
       ),
       body: Column(
         children: [
