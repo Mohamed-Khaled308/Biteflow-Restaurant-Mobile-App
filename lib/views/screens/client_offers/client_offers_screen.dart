d308f8665414184b76c62ccdd2965e1ef20011fd
Abdelrahman Salah <abshehata11@gmail.com>
2024-12-15 03:06:48 +0200

    handling offers UI

--- lib/views/screens/client_offers/client_offers_screen.dart
+++ lib/views/screens/client_offers/client_offers_screen.dart
@@ -5,6 +5,7 @@ import 'package:biteflow/services/firestore/user_service.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:biteflow/core/constants/firestore_collections.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class ClientOffersScreen extends StatefulWidget {
@@ -24,15 +25,15 @@ class _ClientOffersScreenState extends State<ClientOffersScreen> {
       final user = userProvider.user;
 
       if (user != null) {
-          final updatedUser = Client(
-                  id: user.id,
-                  name: user.name,
-                  email: user.email,
-                  fcmToken: user.fcmToken,
-                  orderIds: (user as Client).orderIds,
-                  unseenOfferCount: 0,
-                );        
-          await UserService().updateUser(updatedUser);
+        final updatedUser = Client(
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          fcmToken: user.fcmToken,
+          orderIds: (user as Client).orderIds,
+          unseenOfferCount: 0,
+        );
+        await UserService().updateUser(updatedUser);
       }
     });
   }
@@ -42,6 +43,7 @@ class _ClientOffersScreenState extends State<ClientOffersScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Offers'),
+        backgroundColor: Theme.of(context).primaryColor,
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
@@ -65,46 +67,76 @@ class _ClientOffersScreenState extends State<ClientOffersScreen> {
             return OfferNotification.fromData(data);
           }).toList();
 
-          return ListView.builder(
+          return GridView.builder(
             padding: const EdgeInsets.all(16.0),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1, // Number of columns
+              crossAxisSpacing: 12.0,
+              mainAxisSpacing: 12.0,
+              childAspectRatio:
+                  4 / 1, // Adjusted ratio for better content layout
+            ),
             itemCount: offers.length,
             itemBuilder: (context, index) {
               final offer = offers[index];
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 12.0),
+              return GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Offer Title'),
+                        content: Text(offer.title),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
                 child: Card(
+                  elevation: 4.0,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12.0),
                   ),
-                  elevation: 3.0,
                   child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Container(
-                          width: 40.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                            color: Colors.blueAccent,
-                            borderRadius: BorderRadius.circular(20.0),
+                        Text(
+                          offer.title,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
                           ),
-                          child: const Icon(
-                            Icons.restaurant_menu,
-                            color: Colors.white,
-                            size: 20.0,
-                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        const SizedBox(width: 16.0),
-                        Expanded(
-                          child: Text(
-                            offer.title,
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black87,
+                        const Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Valid Until:',
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.grey.shade600,
+                              ),
                             ),
-                          ),
+                            Text(
+                              DateFormat('MMM dd, yyyy').format(offer.endDate),
+                              style: const TextStyle(
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.deepOrange,
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
